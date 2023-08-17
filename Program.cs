5015659a770c10c13460f3f5b9523241d157e1c4
Ashar Ullah <Ashar@DESKTOP-Q9D5124>
2023-08-18 00:51:52 +0500

    Check now

--- Program.cs
+++ Program.cs
@@ -26,7 +26,6 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-
         List<NewCarService> carServiceDataList = new();
         List<CarServices> carServicesList = new();
 
@@ -37,7 +36,7 @@ internal class Program
                     x.ServiceId == carServiceData.ServiceId &&
                     x.ServiceTypeId == carServiceData.ServiceTypeId &&
                     x.ServiceName == carServiceData.ServiceName
-                    );
+                );
 
             if (existingCarService == null)
             {
@@ -48,27 +47,69 @@ internal class Program
                     ServiceName = carServiceData.ServiceName,
                     listSubServiceName = new List<SubServices?>()
                 });
+
             }
         }
-
-        foreach (var carServiceData in carServiceDataList)
+        foreach (var item in carServiceDataList)
         {
-            var carService = carServicesList
-                .FirstOrDefault(x =>
-                    x.ServiceId == carServiceData.SubServiceId);
-
-            if (carService != null)
+            foreach (var subServiceName in carServicesList)
             {
-                foreach (var subServiceName in carServiceDataList)
+                if (subServiceName.ServiceId.Equals(item.ServiceId))
                 {
-                    carService.listSubServiceName?.Add(new SubServices
+                    subServiceName.listSubServiceName?.Add(new SubServices
                     {
-                        SubServiceId = subServiceName.SubServiceId,
-                        SubServiceName = subServiceName.SubServiceName
+                        SubServiceId = item.SubServiceId,
+                        SubServiceName = item.SubServiceName
                     });
                 }
             }
         }
+        #region Old Version Error In Condition
+
+        //List<NewCarService> carServiceDataList = new();
+        //List<CarServices> carServicesList = new();
+
+        //foreach (var carServiceData in carServiceDataList)
+        //{
+        //    var existingCarService = carServicesList
+        //        .FirstOrDefault(x =>
+        //            x.ServiceId == carServiceData.ServiceId &&
+        //            x.ServiceTypeId == carServiceData.ServiceTypeId &&
+        //            x.ServiceName == carServiceData.ServiceName
+        //            );
+
+        //    if (existingCarService == null)
+        //    {
+        //        carServicesList.Add(new CarServices
+        //        {
+        //            ServiceId = carServiceData.ServiceId,
+        //            ServiceTypeId = carServiceData.ServiceTypeId,
+        //            ServiceName = carServiceData.ServiceName,
+        //            listSubServiceName = new List<SubServices?>()
+        //        });
+        //    }
+        //}
+
+        //foreach (var carServiceData in carServiceDataList)
+        //{
+        //    var carService = carServicesList
+        //        .FirstOrDefault(x =>
+        //            x.ServiceId == carServiceData.SubServiceId);
+
+        //    if (carService != null)
+        //    {
+        //        foreach (var subServiceName in carServiceDataList)
+        //        {
+        //            carService.listSubServiceName?.Add(new SubServices
+        //            {
+        //                SubServiceId = subServiceName.SubServiceId,
+        //                SubServiceName = subServiceName.SubServiceName
+        //            });
+        //        }
+        //    }
+        //}
+        #endregion
+
     }
 
 
